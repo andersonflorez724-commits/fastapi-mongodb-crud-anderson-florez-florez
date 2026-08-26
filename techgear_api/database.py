cccbe6c5bad1dd1e433d907636b79201c5cc3091
Anderson Florez Florez <andersonflorez724@gmail.com>
2026-08-25 19:42:13 -0500

    fix: usa variable de entorno MONGODB_DB en lugar de nombre hardcodeado
    Ahora el nombre de la base de datos se configura mediante la variable
    de entorno MONGODB_DB (definida en .env.example), en lugar de estar
    hardcodeado como 'tienda_db' en database.py.

--- techgear_api/database.py
+++ techgear_api/database.py
@@ -5,8 +5,9 @@ from dotenv import load_dotenv
 load_dotenv()
 
 MONGODB_URL = os.getenv("MONGODB_URL")
+MONGODB_DB = os.getenv("MONGODB_DB", "techgear_db")
 client = AsyncIOMotorClient(MONGODB_URL)
 
-database = client.tienda_db
+database = client[MONGODB_DB]
 productos_collection = database.get_collection("productos")
 pedidos_collection = database.get_collection("pedidos")
